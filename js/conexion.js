87c7bdfc68bef1925beaa1cd4ac08c4bea6ed7b7
loep1039 <213527042+loep1039@users.noreply.github.com>
2026-04-15 10:55:38 -0400

    Create conexion.js

---
+++ js/conexion.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDNQlaP3mrYDd4Gz0pHS-sXhjY7ZWk2TfW",
+  authDomain: "tecnosistemas-v.firebaseapp.com",
+  projectId: "tecnosistemas-v",
+  storageBucket: "tecnosistemas-v.firebasestorage.app",
+  messagingSenderId: "256764716518",
+  appId: "1:256764716518:web:29589e84f9ae3ded7fe227",
+  measurementId: "G-X83LM1BNTC"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
